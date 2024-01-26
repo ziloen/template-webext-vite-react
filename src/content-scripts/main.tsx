e3d5f677f6267b9ec5bbeff9b3c0ac5d286bfe06
N <34268416+ziloen@users.noreply.github.com>
2024-01-26 10:18:22 +0800

    Update import

--- src/content-scripts/main.tsx
+++ src/content-scripts/main.tsx
@@ -1,7 +1,5 @@
 import '~/utils/polyfill'
 
-import styles from '~/styles/tailwind.css?inline'
-
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
@@ -9,9 +7,12 @@ const div = document.createElement('div')
 const shadow = div.attachShadow({ mode: 'closed' })
 
 const styleSheet = new CSSStyleSheet()
-styleSheet.replaceSync(styles)
 shadow.adoptedStyleSheets = [styleSheet]
 
-// document.body.append(div)
+document.body.append(div)
 
 createRoot(shadow).render(<App />)
+
+import('~/styles/tailwind.css?inline').then(m => {
+  styleSheet.replaceSync(m.default)
+})
