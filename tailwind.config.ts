0ef10145f2fda2cf8fb15b9633f29476d2b5dea1
N <34268416+ziloen@users.noreply.github.com>
2024-03-05 15:44:45 +0800

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,41 +1,10 @@
-import { pluginCreator } from '@ziloen/tailwind-config'
+import { preset } from '@ziloen/tailwind-config'
 import type { Config } from 'tailwindcss'
-import type { PluginAPI } from 'tailwindcss/types/config'
 
 export default {
   content: ['./src/**/*.{ts,tsx,html}'],
+  presets: [preset],
   theme: {
-    colors: {
-      // Default colors
-      inherit: 'inherit',
-      current: 'currentColor',
-      transparent: 'transparent',
-      black: '#000',
-      white: '#fff',
-    },
-
-    lineHeight: {
-      none: '1',
-    },
-
-    zIndex: {
-      auto: 'auto',
-      0: '0',
-      1: '1',
-      2: '2',
-      3: '3',
-      max: '2147483647',
-    },
-
     extend: {},
   },
-  experimental: {
-    // Remove unused global css variables, e.g. --tw-translate-x: 0;
-    optimizeUniversalDefaults: true,
-    // matchVariant: true,
-  },
-  // https://tailwindcss.com/docs/theme#configuration-reference
-  // https://github.com/tailwindlabs/tailwindcss/blob/master/src/corePlugins.js
-  corePlugins: {},
-  plugins: [pluginCreator],
 } satisfies Config
